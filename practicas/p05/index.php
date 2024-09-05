eb18811efb3cd6744d0fee1bae7257b506ace481
Lalo <eduar.egm65@gmail.com>
2024-09-04 20:13:22 -0600

    Ejercicio 2

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -7,4 +7,18 @@
     $_element1 = '$_element1';
     //$house*5 = '$house*5';
     echo $_myvar.'<br>'.$_7var.'<br>'.$myvar.'<br>'.$var7.'<br>'.$_element1.'<br>';
+
+    echo '<br>';
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
+    echo $a.'<br>'.$b.'<br>'.$c.'<br>';
+
+    echo '<br>';
+    $a = "PHP server";
+    $b = &$a;
+    echo $a.'<br>'.$b.'<br>';
+    echo 'Tras la segunda asignación se cambian los valores de las variables 
+    $a y $b, la variable $a solo cambia su valor mientra que la variable $b obtiene el 
+    valor de la nueva variable $a <br>';
 ?>
