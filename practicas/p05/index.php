2438cc4f84e6f63f341c9d41cbc5a3d9d99677a5
Lalo <eduar.egm65@gmail.com>
2024-09-04 19:10:26 -0600

    Ejercicio 1

---
+++ practicas/p05/index.php
@@ -0,0 +1,10 @@
+<?php
+    $_myvar = '$_myvar';
+    $_7var = '$_7var';
+    //myvar = 'myvar';
+    $myvar = '$myvar';
+    $var7 = '$var7';
+    $_element1 = '$_element1';
+    //$house*5 = '$house*5';
+    echo $_myvar.'<br>'.$_7var.'<br>'.$myvar.'<br>'.$var7.'<br>'.$_element1.'<br>';
+?>
