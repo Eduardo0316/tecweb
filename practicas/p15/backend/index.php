76e526882b6b70382557a39ef8b2f63367c3cbb1
Lalo <eduar.egm65@gmail.com>
2024-12-02 15:23:34 -0600

    Actividad xml, p14 y p15

---
+++ practicas/p15/backend/index.php
@@ -0,0 +1,113 @@
+<?php
+    require_once '../vendor/autoload.php';
+
+    use myapi\Read as Read;
+    use myapi\Create as Create;
+    use myapi\Delete as Delete;
+    use myapi\Update as Update;
+    
+    $app = new Slim\App();
+
+    $app->get('/list', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Read("root", "mendoza21:)", "marketzone");
+
+        $products->list();
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->post('/add', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Create("root", "mendoza21:)", "marketzone");
+
+        $reqPost = $request->getParsedBody();
+
+        if(isset($reqPost['nombre'])) {
+            $products->add($reqPost);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->post('/edit', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Update("root", "mendoza21:)", "marketzone");
+
+        $reqPost = $request->getParsedBody();
+
+        if(isset($reqPost['id'])) {
+            $products->edit($reqPost);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->post('/delete', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Delete("root", "mendoza21:)", "marketzone");
+
+        $reqPost = $request->getParsedBody();
+
+        if(isset($reqPost['id'])) {
+            $products->delete($reqPost['id']);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->get('/search', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Read("root", "mendoza21:)", "marketzone");
+
+        $queryParams = $request->getQueryParams();
+
+        if(isset($queryParams['search'])) {
+            $products->search($queryParams['search']);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->post('/single', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Read("root", "mendoza21:)", "marketzone");
+
+        $reqPost = $request->getParsedBody();
+
+        if(isset($reqPost['id'])) {
+            $products->single($reqPost['id']);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->post('/singleSearch', function($request, $response, $args){
+        $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8'); //Para los acentos
+
+        $products = new Read("root", "mendoza21:)", "marketzone");
+
+        $reqPost = $request->getParsedBody();
+
+        if(isset($reqPost['nombre'])) {
+            $products->singleSearch($reqPost);
+        }
+
+        $response->getBody()->write(json_encode($products->getData()));
+        return $response;
+    });
+
+    $app->run();
+?>
